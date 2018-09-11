6e0ea0db45edde9eb895a072413e0e5df048356b
aleksei.bulgak <a.bulgak@intetics.com>
2018-09-12 00:14:41 +0300

    added implementation for Sorter class

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.elements = [];
+    this.compareFunction = (l, r) => l - r;
   }
 
   add(element) {
     // your implementation
+    this.elements.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.elements[index]
   }
 
   get length() {
     // your implementation
+    return this.elements.length;
   }
 
   toArray() {
     // your implementation
+    return this.elements;
   }
 
   sort(indices) {
     // your implementation
+    var arrayForSorting = [];
+    indices.forEach(element => {
+      arrayForSorting.push(this.elements[element]);
+    });
+    arrayForSorting.sort(this.compareFunction);
+    indices.sort((l, r) => l - r)
+      .forEach((element, index) => {
+        this.elements[element] = arrayForSorting[index]
+      });
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
